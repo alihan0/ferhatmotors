ce6df83d8cc59944e26bfe4f4bd1be80f2a67ca4
Alihan Öztürk <alihanozturk364@gmail.com>
2023-08-20 20:05:09 +0300

    customer doc added

--- resources/views/layout/docs/how-to-use.blade.php
+++ resources/views/layout/docs/how-to-use.blade.php
@@ -24,27 +24,69 @@
 </div>
 
 <div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="display-5 fw-bold text-body-emphasis">Centered hero</h1>
-    <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
-      </div>
+    <h4 class="display-7 fw-bold text-body-emphasis mb-4">Kullanıcı Yönetimi</h4>
+    <div class="col-lg-6 mx-auto text-start">
+      <p class="mb-4">
+        Sistem, <code>Kullanıcı</code> hesapları üzerinden yürütülür. Kullanıcılar, bu sistem üzerinde oturum açma ve işlem yapma yetkisine sahip olan kişilerdir.
+      </p>
+
+      <p class="mb-4">
+        Sistemi kullanmak için bir kullanıcı hesabına ihtiyacınız var. Sistem size vir adet varsayılan kullanıcı ile teslim edildi. Bu varsayılan kullanıcı ile oturum açtıktan sonra yapmanız gereken ilk kiş kendiniz için bir kullanıcı oluşturmak ve varsayılan demo kullanıcısını silmektir. Silme işlemi ardından oturumunuzu kapatın ve yeni oluşturduğunuz kullanıcı ile giriş yapın. Bu sayede oturumunuz yenilenmiş olacak ve yaptığınız değişiklikler yeni kullanıcı adına kaydedilecek.
+      </p>
+
+      <p class="mb-4">
+        Sistem, kullanıcı aksiyonlarını kayıt altına almaktadır. Bir ilan oluşturduğunuzda ya da ilan üzerinde bir değişiklik yaptığınızda bu işlemi kimin yaptığını bu kullanıcı aksiyonları aracılığı ile görüntüleyebilirsiniz. Sistemde oturum acabilecek tüm kullanıcıları görüntülemek için <code>/user/all</code> sayfasını ziyaret edin. Bu sayfada şu işlemleri yapabilirsiniz:
+      </p>
+      <ul>
+        <li>Kullanıcı Bilgilerini Görme</li>
+        <li>Kullanıcı Silme</li>
+        <li>Kullanıcının şifresini değiştirme</li>
+      </ul>
+
+      <p class="mb-4">Sistemde oturum açabileceke yeni bir kullanıcı oluşturmak için <code>/user/add</code> sayfasını ziyaret edin. Burada oluşturduğunuz bilgiler ile yeni kullanıcı girişi yapılabilir.</p>
+
+      <p class="mb-4">
+        <b>Not:</b> Kullanıcı şifreleri <code>Laravel Hash</code> yöntemi ile saklanmaktadır. Bu yöntem sayesinde şifreler özel olarak kriptolanır. Şifreyi sadece oluştururken siz göreceksiniz ve sadece sizin paylaştığınız kanallarda kalacak. Şifreyi daha sonra değiştirebilirsiniz fakat şifreyi görüntüleyebilmeniz mümkün değildir. Şifre güvenliğini sağlamak sizin sorumluluğunuzdadır.
+      </p>
+
+
     </div>
   </div>
   <div class="b-example-divider"></div>
 
   <div class="px-4 py-5 my-5 text-center">
-    <img class="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="display-5 fw-bold text-body-emphasis">Centered hero</h1>
+    <h4 class="display-7 fw-bold text-body-emphasis mb-4">Müşteri Yönetimi</h4>
+    <div class="col-lg-6 mx-auto text-start">
+      <p class="mb-4">
+        Müşteriler rehber oluşturmak için kullanılır. Tüm müşteri datasını burada bulabilirsiniz. Telefon ve e-postaları ile iletişime geçebilmeniz için tasarlanmıştır. Buradan <code>Düzenle</code> butonuna tıklayarak açılan modaldan müşteri bilgilerini düzenleyebilirsiniz. Müşteri listenize ulaşmak için <code>/customer/all</code> sayfasına gidin. Yeni bir müşteri eklemek istiyorsanız <code>/customer/new</code> sayfasını ziyaret edin.
+      </p>
+    </div>
+  </div>
+
+  <div class="b-example-divider"></div>
+
+  <div class="px-4 py-5 my-5 text-center">
+    <h4 class="display-7 fw-bold text-body-emphasis mb-4">İlan Yönetimi</h4>
     <div class="col-lg-6 mx-auto">
       <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
-      </div>
+    </div>
+  </div>
+
+  <div class="b-example-divider"></div>
+
+  <div class="px-4 py-5 my-5 text-center">
+    <h4 class="display-7 fw-bold text-body-emphasis mb-4">Rapor Yönetimi</h4>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+    </div>
+  </div>
+
+  <div class="b-example-divider"></div>
+
+  <div class="px-4 py-5 my-5 text-center">
+    <h4 class="display-7 fw-bold text-body-emphasis mb-4">Sistem Ayarları</h4>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
     </div>
   </div>
 
