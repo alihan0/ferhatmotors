b662fe39834ff3acd8d4bebf6f15e6fef0c4066e
Alihan Öztürk <alihanozturk364@gmail.com>
2023-08-22 11:58:54 +0300

    expense migration created

---
+++ database/migrations/2023_08_22_085729_create_expense.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expense', function (Blueprint $table) {
+            $table->id();
+            $table->integer('advert');
+            $table->integer('user');
+            $table->string('type');
+            $table->decimal('amouny',9,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expense');
+    }
+};
