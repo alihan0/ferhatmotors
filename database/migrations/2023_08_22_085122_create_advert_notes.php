1bd6f65fed306cd6f10970bafec392f2d039443a
Alihan Öztürk <alihanozturk364@gmail.com>
2023-08-22 11:52:19 +0300

    advert note migrtion created

---
+++ database/migrations/2023_08_22_085122_create_advert_notes.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('advert_notes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('advert');
+            $table->integer('user');
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('advert_notes');
+    }
+};
